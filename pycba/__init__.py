b190dd860b5f2d95f91d60e1d0c2610ea2b37c28
Colin Caprani <colin.caprani@monash.edu>
2022-02-07 01:34:48 +1100

    Package setup and workflows

--- pycba/__init__.py
+++ pycba/__init__.py
@@ -2,6 +2,8 @@
 PyCBA - Continuous Beam Analysis in Python
 """
 
+__version__ = "0.1.1"
+
 from .analysis import *
 from .beam import *
 from .load import *
@@ -9,3 +11,5 @@ from .results import *
 from .inf_lines import *
 from .utils import *
 from .bridge import *
+
+
